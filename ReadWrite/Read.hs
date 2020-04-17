fc837cc1f538c99a27acceef36ff6199d179ff07
Beliaev <beliaevs@gmail.com>
2020-04-18 02:21:00 +0300

    Read command line argument for input file

--- ReadWrite/Read.hs
+++ ReadWrite/Read.hs
@@ -1,15 +1,23 @@
 import Data.List
+import System.Environment
 
 main :: IO ()
 main = do
-   contents <- readFile "test.txt";
-   print . sumAndMax . map readInt . words $ contents
+    args <- getArgs
+    case args of
+        [filename] -> 
+            do
+                contents <- readFile filename
+                let (summa, m) = sumAndMax . map readInt . words $ contents
+                putStrLn ("sum = " ++ show summa ++ ", max = " ++ show m)
+        _ -> putStrLn "Usage: Read.exe [filename]"
 
 
 sumAndMax :: [Int] -> (Int, Int)
-sumAndMax = let 
-    sm (n1, m1) k = (n1 + k, max m1 k) 
-            in foldl' sm (0, 0) 
+sumAndMax = 
+    let 
+        sm (n1, m1) k = (n1 + k, max m1 k) 
+    in  foldl' sm (0, 0) 
 
 
 readInt :: String -> Int
